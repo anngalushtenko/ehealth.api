52b1716f65b8af649de263daad2fcaa1f2558bd0
Maksym Ivanov <imax.iva@gmail.com>
2019-02-20 14:50:34 +0200

    credo warnings fixed

--- apps/core/lib/core/kafka/producer.ex
+++ apps/core/lib/core/kafka/producer.ex
@@ -7,7 +7,8 @@ defmodule Core.Kafka.Producer do
   require Logger
 
   def publish_deactivate_declaration_event(event) do
-    with :ok <- Kaffe.Producer.produce_sync(@deactivate_declaration_events_topic, 0, nil, :erlang.term_to_binary(event)) do
+    with :ok <-
+           Kaffe.Producer.produce_sync(@deactivate_declaration_events_topic, 0, nil, :erlang.term_to_binary(event)) do
       Logger.info("Published event #{inspect(event)} to kafka", application: :kaffe)
       :ok
     end
